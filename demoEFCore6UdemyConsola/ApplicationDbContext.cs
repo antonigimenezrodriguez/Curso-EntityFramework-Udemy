8cf7a78817a96f958f7172493847da788c73dea6
Antoni Gimenez <antoni.gimenez@outlook.es>
2023-05-02 16:28:57 +0200

    Ejemplo en Aplicación de Consola

---
+++ demoEFCore6UdemyConsola/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoEFCore6Udemy
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=DemoEFCore6Udemy;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<Persona> Personas { get; set; } 
+    }
+}
---
+++ demoEFCore6UdemyConsola/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace demoEFCore6Udemy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
