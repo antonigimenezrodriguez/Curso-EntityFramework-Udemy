0468daae50b90c11f086e80122bc7e922b249813
Antoni Gimenez <antoni.gimenez@outlook.es>
2023-05-02 19:16:53 +0200

    Lección 26: enums

--- demoEFCore6UdemyASPNET/EFCorePeliculas/Entidades/SalaDeCine.cs
+++ demoEFCore6UdemyASPNET/EFCorePeliculas/Entidades/SalaDeCine.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public decimal Precio { get; set; }
+        public TipoSalaDeCine TiposalaDeCine { get; set; }
 
         //FK
         public int CineId { get; set; }
---
+++ demoEFCore6UdemyASPNET/EFCorePeliculas/Migrations/20230502171620_TipoSalaDeCine.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCorePeliculas.Migrations
+{
+    public partial class TipoSalaDeCine : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TiposalaDeCine",
+                table: "SalasDeCine",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TiposalaDeCine",
+                table: "SalasDeCine");
+        }
+    }
+}
